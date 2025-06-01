8e8f82b370ab7e429761e407b42fd502f7810e7d
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-01 09:19:45 -0300

    fix: @RequestMapping('/movieprime/***') >> /moviesprime/***

--- src/main/java/com/moviesPrime/controller/CategoryController.java
+++ src/main/java/com/moviesPrime/controller/CategoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movieprime/category")
+@RequestMapping("/moviesprime/category")
 @RequiredArgsConstructor
 public class CategoryController {
 
--- src/main/java/com/moviesPrime/controller/StreamingController.java
+++ src/main/java/com/moviesPrime/controller/StreamingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/movieprime/streaming")
+@RequestMapping("/moviesprime/streaming")
 @RequiredArgsConstructor
 public class StreamingController {
     private final StreamingService streamingService;
