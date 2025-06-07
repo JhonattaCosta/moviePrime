682f52414208be1f5e8e4388d421627dd8424262
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-07 12:44:52 -0300

    docs: add swagger in movie

--- src/main/java/com/moviesPrime/controller/MovieController.java
+++ src/main/java/com/moviesPrime/controller/MovieController.java
@@ -5,6 +5,13 @@ import com.moviesPrime.controller.response.MovieResponse;
 import com.moviesPrime.entity.Movie;
 import com.moviesPrime.mapper.MovieMapper;
 import com.moviesPrime.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +23,25 @@ import java.util.Optional;
 @RestController
 @RequestMapping("moviesprime/movie")
 @RequiredArgsConstructor
+@Tag(name = "Movie", description = "Recurso responsavel pelo gerenciamento de filmes")
 public class MovieController {
     private final MovieService movieService;
 
+
+    @Operation(summary = "Salvar filme", description = "esse metodo salva um filme",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "201", description = "Filme salvo com sucesso",
+            content = @Content(schema = @Schema(implementation = MovieRequest.class)))
     @PostMapping
-    public ResponseEntity<MovieResponse> saveMovie(@RequestBody MovieRequest request) {
+    public ResponseEntity<MovieResponse> saveMovie(@Valid @RequestBody MovieRequest request) {
         Movie savedMovie = movieService.saveMovie(MovieMapper.toMovie(request));
         return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
     }
 
+    @Operation(summary = "Listar Filmes", description = "Lista todos os filmes cadastrados",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "202", description = "Filmes listados",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @GetMapping
     public ResponseEntity<List<MovieResponse>> findAllMovies(){
         return ResponseEntity.ok(movieService.findAllMovies()
@@ -34,13 +51,20 @@ public class MovieController {
 
     }
 
+    @Operation(summary = "Listar um Filme", description = "Lista filme por ID",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "202", description = "Filme listado por ID",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @GetMapping("/{id}")
     public ResponseEntity<MovieResponse> findMovieId(@PathVariable Long id){
         return movieService.findMovieId(id)
                 .map( movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
                 .orElse(ResponseEntity.notFound().build());
     }
-
+    @Operation(summary = "Procurar filme por categoria", description = "Lista filmes pela categoria cadastrada",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Filmes listados pela categoria",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @GetMapping("/search")
     public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category){
         return ResponseEntity.ok(movieService.findByCategory(category)
@@ -50,13 +74,21 @@ public class MovieController {
 
     }
 
+    @Operation(summary = "Update filme", description = "Atualiza o filme escolhido",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "200", description = "Atualizar filmes selecionados por ID",
+            content = @Content(schema = @Schema(implementation = MovieRequest.class)))
     @PutMapping("/{id}")
-    public ResponseEntity<MovieResponse> updateMovieId(@PathVariable Long id,@RequestBody MovieRequest request){
+    public ResponseEntity<MovieResponse> updateMovieId(@PathVariable Long id, @Valid  @RequestBody MovieRequest request){
         return movieService.updateMovieId(id, MovieMapper.toMovie(request))
                 .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deletar filme", description = "Deleta o filme",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponse(responseCode = "204", description = "Deleta o filme por id",
+            content = @Content(schema = @Schema(implementation = MovieResponse.class)))
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMovieId (@PathVariable Long id){
         Optional<Movie> optMovie = movieService.findMovieId(id);
--- src/main/java/com/moviesPrime/controller/request/MovieRequest.java
+++ src/main/java/com/moviesPrime/controller/request/MovieRequest.java
@@ -1,14 +1,20 @@
 package com.moviesPrime.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public record MovieRequest(String title,
+public record MovieRequest(@Schema(type = "String", description = "Titulo do Filme")
+                           @NotEmpty(message = "Titulo do filme é obrigatório!") String title,
+                           @Schema(type = "String", description = "Descrição do filme")
                            String description,
+                           @Schema(type = "LocalDate", description = "Data de lançamento do filme")
                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
+                           @Schema(type = "double", description = "Nota do filme")
                            double rating,
                            List<Long> categories,
                            List<Long> streamings
