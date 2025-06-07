e9a188b6bb04b5b53188529262e836491a9c204f
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-07 11:32:35 -0300

    feat: add endpoint login

--- src/main/java/com/moviesPrime/controller/AuthController.java
+++ src/main/java/com/moviesPrime/controller/AuthController.java
@@ -1,17 +1,27 @@
 package com.moviesPrime.controller;
 
+import com.moviesPrime.config.TokenService;
+import com.moviesPrime.controller.request.LoginRequest;
 import com.moviesPrime.controller.request.UserRequest;
+import com.moviesPrime.controller.response.LoginResponse;
 import com.moviesPrime.controller.response.UserResponse;
 import com.moviesPrime.entity.User;
+import com.moviesPrime.exception.UsernameOrPasswordInvalidException;
 import com.moviesPrime.mapper.UserMapper;
 import com.moviesPrime.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/moviesprime/auth")
@@ -19,11 +29,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest request){
         User savedUser = userService.saveUser(UserMapper.toUser(request));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
+        try{
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(), request.password());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        User user = (User) authenticate.getPrincipal();
+
+        String token = tokenService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
+        }catch (BadCredentialsException e){
+            throw new UsernameOrPasswordInvalidException("Usuário ou senha inválido.");
+        }
+
     }
 
 }
