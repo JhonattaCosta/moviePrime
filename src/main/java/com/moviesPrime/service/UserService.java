68866e99ea6b5765df29137e9be6bd77d600f9c2
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-08 12:33:33 -0300

    feat: add userEntity/Repository/Service

--- src/main/java/com/moviesPrime/service/UserService.java
+++ src/main/java/com/moviesPrime/service/UserService.java
@@ -3,6 +3,7 @@ package com.moviesPrime.service;
 import com.moviesPrime.entity.User;
 import com.moviesPrime.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 
