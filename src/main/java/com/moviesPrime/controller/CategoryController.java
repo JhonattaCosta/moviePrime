86c856210182af1cf1f16aaa0c56d4cd0f333799
JhonattaCosta <jhonattasantos27@gmail.com>
2025-06-15 19:49:29 -0300

    doc: add Swagger doc

--- src/main/java/com/moviesPrime/controller/CategoryController.java
+++ src/main/java/com/moviesPrime/controller/CategoryController.java
@@ -6,6 +6,9 @@ import com.moviesPrime.controller.response.CategoryResponse;
 import com.moviesPrime.entity.Category;
 import com.moviesPrime.mapper.CategoryMapper;
 import com.moviesPrime.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,11 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "Listar Categorias", description = "Lista todas as categorias")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Lista de categoria"),
+            @ApiResponse(responseCode = "400", description = "Não foi possivel listar categorias")
+    })
     @GetMapping()
     public ResponseEntity<List<CategoryResponse>> getAllCategory(){
         return  ResponseEntity.ok(categoryService.findAll()
@@ -30,6 +38,11 @@ public class CategoryController {
                 .toList());
     }
 
+    @Operation(summary = "Listar Categorias por id", description = "Lista categorias pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Categoria listada por id"),
+            @ApiResponse(responseCode = "400", description = "Não foi possivel encontra a categoria")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponse> getCategoryId(@PathVariable Long id){
         return categoryService.findCategoryId(id)
@@ -37,6 +50,11 @@ public class CategoryController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Salvar Categoria", description = "sava a categoria no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "categoria salva"),
+            @ApiResponse(responseCode = "400", description = "Não foi possivel salvar categoria")
+    })
     @PostMapping
     public ResponseEntity<CategoryResponse> saveCategory(@Valid @RequestBody CategoryRequest request){
         Category savedCategory = categoryService.saveCategory(CategoryMapper.toCategory(request));
@@ -44,6 +62,11 @@ public class CategoryController {
                 .body(CategoryMapper.toCategoryResponse(savedCategory));
     }
 
+    @Operation(summary = "Atualizar Categoria", description = "atualiza a categoria no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "categoria atualizada por id"),
+            @ApiResponse(responseCode = "400", description = "Não foi possivel atualizar categoria")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponse> updateCategory(@PathVariable Long id,@Valid @RequestBody CategoryRequest request){
         return categoryService.updateCategory(id, CategoryMapper.toCategory(request))
@@ -51,6 +74,11 @@ public class CategoryController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Deleta Categoria", description = "deleta a categoria no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deleta a categoria por id"),
+            @ApiResponse(responseCode = "400", description = "Não foi possivel deletar categoria")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategoryId(@PathVariable Long id){
         Optional<Category> optCategory = categoryService.findCategoryId(id);
